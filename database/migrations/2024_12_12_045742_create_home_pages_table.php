5264d719dc4a2623c44b55f199d1c5870907fc44
shahed141123 <khandkershahed23@gmail.com>
2024-12-12 11:23:56 +0600

    08-12-24

--- database/migrations/2024_12_12_045742_create_home_pages_table.php
+++ database/migrations/2024_12_12_045742_create_home_pages_table.php
@@ -13,6 +13,22 @@ return new class extends Migration
     {
         Schema::create('home_pages', function (Blueprint $table) {
             $table->id();
+            $table->string('row_one_badge')->nullable();
+            $table->string('row_one_title')->nullable();
+            $table->text('row_one_description')->nullable();
+            $table->text('row_one_link')->nullable();
+            $table->string('row_two_icon_one')->nullable();
+            $table->string('row_two_icon_one_title')->nullable();
+            $table->string('row_two_icon_two')->nullable();
+            $table->string('row_two_icon_two_title')->nullable();
+            $table->string('row_two_icon_three')->nullable();
+            $table->string('row_two_icon_three_title')->nullable();
+            $table->string('row_two_icon_four')->nullable();
+            $table->string('row_two_icon_four_title')->nullable();
+            $table->string('row_three_title')->nullable();
+            $table->string('row_three_pdf')->nullable();//PDF
+            $table->string('row_four_title')->nullable();
+            $table->text('row_four_description')->nullable();
             $table->timestamps();
         });
     }
