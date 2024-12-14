987a815c9c0c8a399eecda5232af35dee340306a
shahed141123 <khandkershahed23@gmail.com>
2024-12-14 10:31:14 +0600

    08-12-24

--- routes/frontend.php
+++ routes/frontend.php
@@ -1,10 +1,13 @@
 <?php
 
 use App\Http\Controllers\Controller;
+use App\Http\Controllers\Frontend\FrontendController;
 use App\Http\Controllers\Frontend\HomePageController;
 use Illuminate\Support\Facades\Route;
 
 //Contact Section
+
+Route::get('/', [FrontendController::class, 'home'])->name('home');
 Route::get('/contact', [HomePageController::class, 'contact'])->name('contact');
 Route::post('/contact-store', [HomePageController::class, 'contactStore'])->name('contact.store');
 
