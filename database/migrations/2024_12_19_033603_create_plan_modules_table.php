33fdaaeaf0795001f58b30627bb9ba2055286542
shahed141123 <khandkershahed23@gmail.com>
2024-12-19 10:12:23 +0600

    08-12-24

---
+++ database/migrations/2024_12_19_033603_create_plan_modules_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('plan_modules', function (Blueprint $table) {
+            $table->id();
+            $table->string('type')->nullable(); //selectbox
+            $table->string('title')->nullable();
+            $table->string('slug')->unique()->nullable();
+            $table->string('value')->nullable();
+            $table->string('currency')->nullable(); //selectbox
+            $table->double('price')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('plan_modules');
+    }
+};
---
+++ database/migrations/2024_12_19_033645_create_pricing_plans_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pricing_plans', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable();
+            $table->string('slug')->unique()->nullable();
+            $table->string('duration')->nullable(); //selectbox :- monthly, Yearly
+            $table->string('currency')->nullable(); //selectbox
+            $table->double('price')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pricing_plans');
+    }
+};
