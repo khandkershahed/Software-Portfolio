4356cc4655827602f2c69954f5c38f7c2107d5af
shahed141123 <khandkershahed23@gmail.com>
2025-02-12 17:39:36 +0600

    26-01-25

--- resources/views/frontend/pages/client.blade.php
+++ resources/views/frontend/pages/client.blade.php
@@ -1,17 +1,21 @@
+
 <section class="py-3">
     <div class="container-fluid px-0 py-3">
         <div class="row align-items-center">
             <div>
                 <!-- Partners Logo -->
                 <section class="partners-carousel">
-
+                    <div class="partner-logo-box">
+                        <img class="partners-logo"
+                            src="{{ asset('/') }}assets/images/partners-logo/Dadabhai-Logo.png" alt="" />
+                    </div>
                     @foreach ($company_clients as $company_client)
                         <div class="partner-logo-box">
-                            <img class="partners-logo" src="{{ !empty($company_client->logo) ? url('storage/' . $company_client->logo) : 'https://ui-avatars.com/api/?name=' . urlencode($company_client->name) }}"
+                            <img class="partners-logo"
+                                src="{{ !empty($company_client->logo) ? url('storage/' . $company_client->logo) : asset('images/no_logo.png') }}"
                                 alt="" />
                         </div>
                     @endforeach
-
                 </section>
 
                 <!-- Partners Logo End-->
