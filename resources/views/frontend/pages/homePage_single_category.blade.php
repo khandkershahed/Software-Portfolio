7d8871f4d5d95d9c68d64ef513768912f3991597
shahed141123 <khandkershahed23@gmail.com>
2025-02-19 10:35:55 +0600

    26-01-25

--- resources/views/frontend/pages/homePage_single_category.blade.php
+++ resources/views/frontend/pages/homePage_single_category.blade.php
@@ -1,52 +1,53 @@
-<section class="py-5">
-    <div class="container">
+@if (!empty(optional($categoryThree)->name) || !empty(optional($categoryThree)->description))
+    <section class="py-5">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-6">
+                    <div class="py-5">
+                        <div class="d-flex align-items-center">
+                            <h1 class="design-title text-black">
+                                {{ optional($categoryThree)->name }}
+                            </h1>
+                            @if (!empty(optional($categoryThree)->name))
+                                <span class="line ms-2"></span>
+                            @endif
+                        </div>
+                        <p class="pt-3 text-black">
+                            {!! optional($categoryThree)->description !!}
+                        </p>
+                        <div class="row mt-4">
 
-        <div class="row align-items-center">
-            <div class="col-lg-6">
-                <div class="py-5">
-                    <div class="d-flex align-items-center">
-                        <h1 class="design-title text-black">
-                            {{ optional($categoryThree)->name }}
-                        </h1>
-                        @if (!empty(optional($categoryThree)->name))
-                            <span class="line ms-2"></span>
-                        @endif
-                    </div>
-                    <p class="pt-3 text-black">
-                        {!! optional($categoryThree)->description !!}
-                    </p>
-                    <div class="row mt-4">
+                            @if (!empty(optional($categoryThree)->children))
+                                @foreach (optional($categoryThree)->children as $header_category_child_three)
+                                    <div class="col-lg-6">
 
-                        @if (!empty(optional($categoryThree)->children))
-                            @foreach (optional($categoryThree)->children as $header_category_child_three)
-                                <div class="col-lg-6">
+                                        <div class="mb-3 shape_side_box">
+                                            <a href="{{ route('project') }}" class="btn btn-white shadow-sm arrow-btns">
+                                                <i class="fa-solid fa-chevron-right pe-2"></i>
+                                            </a>
+                                            <a href="{{ route('project') }}"
+                                                class="btn btn-white shadow-sm services-btn w-75 text-start">
+                                                {{ $header_category_child_three->name }}
+                                            </a>
+                                        </div>
 
-                                    <div class="mb-3 shape_side_box">
-                                        <a href="" class="btn btn-white shadow-sm arrow-btns">
-                                            <i class="fa-solid fa-chevron-right pe-2"></i>
-                                        </a>
-                                        <a href=""
-                                            class="btn btn-white shadow-sm services-btn w-75 text-start">
-                                            {{ $header_category_child_three->name }}
-                                        </a>
                                     </div>
+                                @endforeach
 
-                                </div>
-                            @endforeach
+                            @endif
 
-                        @endif
-
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-lg-6">
+                <div class="col-lg-6">
 
-                <img class="mobile-imgs"
-                    src="{{ !empty(optional($categoryThree)->image) ? url('storage/' . optional($categoryThree)->image) : '' }}"
-                    alt="{{ optional($categoryThree)->name }}" />
+                    <img class="mobile-imgs"
+                        src="{{ !empty(optional($categoryThree)->image) ? url('storage/' . optional($categoryThree)->image) : '' }}"
+                        alt="{{ optional($categoryThree)->name }}" />
 
+                </div>
             </div>
+
         </div>
-
-    </div>
-</section>
+    </section>
+@endif
