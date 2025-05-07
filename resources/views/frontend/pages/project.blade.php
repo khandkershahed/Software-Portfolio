a65c31d5ee212487e1ad682b95ec1d8421d5d80e
shahed141123 <khandkershahed23@gmail.com>
2025-05-07 16:15:10 +0600

    07-05-2025

--- resources/views/frontend/pages/project.blade.php
+++ resources/views/frontend/pages/project.blade.php
@@ -21,13 +21,14 @@
         <div class="container my-5 mb-0">
             <!-- Tab navigation for categories -->
             <ul class="nav nav-tabs projects-tabs" id="myTab" role="tablist">
-                @foreach ($categorys as $index => $category)
-                    @if ($category->projects->count() > 0)
+                @foreach ($categories as $index => $category)
+                    @php $hasProjects = $category->projects->isNotEmpty(); @endphp
+                    @if ($hasProjects)
                         <li class="nav-item" role="presentation">
-                            <a class="nav-link {{ $index === 0 && $category->projects->count() > 0 ? 'active' : '' }}" id="category-{{ $category->id }}-tab"
+                            <a class="nav-link {{ $index === 0 ? 'active' : '' }}" id="category-{{ $category->id }}-tab"
                                 data-bs-toggle="tab" href="#category-{{ $category->id }}" role="tab"
                                 aria-controls="category-{{ $category->id }}"
-                                aria-selected="{{ $index === 0 && $category->projects->count() > 0 ? 'true' : 'false' }}">
+                                aria-selected="{{ $index === 0 ? 'true' : 'false' }}">
                                 {{ $category->name }}
                             </a>
                         </li>
@@ -37,65 +38,43 @@
 
             <!-- Tab content for each category -->
             <div class="tab-content" id="myTabContent">
-                @foreach ($categorys as $index => $category)
-                    <div class="tab-pane fade {{ $index === 0 && $category->projects->count() > 0 ? 'show active' : '' }}" id="category-{{ $category->id }}"
-                        role="tabpanel" aria-labelledby="category-{{ $category->id }}-tab">
-                        <div class="mt-5 row">
-                            @foreach ($category->projects as $project)
-                                {{-- <div class="col-md-6">
-                                    <a href="{{ route('projects.details', $project->slug) }}">
-                                        <div class="mb-3 border-0 shadow-sm card">
-
-                                            <div class="slick-slider-projects" style="height: 300px; overflow:hidden;">
-                                                <div class="image-container">
-                                                    <img src="{{ !empty($project->image_one) ? url('storage/' . $project->image_one) : '' }}"
-                                                        class="card-img-top zoom-effect" alt="dadavaai template one" />
-                                                </div>
-                                                <div class="image-container">
-                                                    <img src="{{ !empty($project->image_two) ? url('storage/' . $project->image_two) : '' }}"
-                                                        class="card-img-top zoom-effect" alt="dadavaai template one" />
-                                                </div>
-                                                <div class="image-container">
-                                                    <img src="{{ !empty($project->image_three) ? url('storage/' . $project->image_three) : '' }}"
-                                                        class="card-img-top zoom-effect" alt="dadavaai template one" />
-                                                </div>
-                                            </div>
-
-                                            <div class="card-body bg-light">
-                                                <h5 class="card-title">{{ $project->name }}</h5>
-                                                <p class="card-text">{{ $project->name }}</p>
-                                            </div>
-                                        </div>
-                                    </a>
-                                </div> --}}
-                                <div class="col-lg-12">
-                                    <div class="section-stack">
-                                        <ul class="stack-cards js-stack-cards">
-                                            <li class="stack-cards__item js-stack-cards__item">
-                                                <a href="{{ route('projects.details', $project->slug) }}">
-                                                    <div class="row align-items-center">
-                                                        <div class="col-lg-6">
-                                                            <div>
-                                                                <img class="img-fluid"
-                                                                    src="{{ !empty($project->image_one) ? url('storage/' . $project->image_one) : '' }}"
-                                                                    alt="">
+                @foreach ($categories as $index => $category)
+                    @php $hasProjects = $category->projects->isNotEmpty(); @endphp
+                    @if ($hasProjects)
+                        <div class="tab-pane fade {{ $index === 0 ? 'show active' : '' }}"
+                            id="category-{{ $category->id }}" role="tabpanel"
+                            aria-labelledby="category-{{ $category->id }}-tab">
+                            <div class="mt-5 row">
+                                @foreach ($category->projects as $project)
+                                    <div class="col-lg-12">
+                                        <div class="section-stack">
+                                            <ul class="stack-cards js-stack-cards">
+                                                <li class="stack-cards__item js-stack-cards__item">
+                                                    <a href="{{ route('projects.details', $project->slug) }}">
+                                                        <div class="row align-items-center">
+                                                            <div class="col-lg-6">
+                                                                <div>
+                                                                    <img class="img-fluid"
+                                                                        src="{{ !empty($project->image_one) ? url('storage/' . $project->image_one) : '' }}"
+                                                                        alt="">
+                                                                </div>
+                                                            </div>
+                                                            <div class="col-lg-6">
+                                                                <h1 class="site-text">{{ $project->name }}</h1>
+                                                                <p class="site-text">
+                                                                    {!! $project->row_one_description !!}
+                                                                </p>
                                                             </div>
                                                         </div>
-                                                        <div class="col-lg-6">
-                                                            <h1 class="site-text">{{ $project->name }}</h1>
-                                                            <p class="site-text">{{ $project->row_one_description }}</p>
-                                                        </div>
-                                                    </div>
-                                                </a>
-                                            </li>
-                                        </ul>
+                                                    </a>
+                                                </li>
+                                            </ul>
+                                        </div>
                                     </div>
-                                </div>
-                            @endforeach
+                                @endforeach
+                            </div>
                         </div>
-
-                    </div>
-                @endforeach
+                    @endforeach
             </div>
         </div>
     </section>
@@ -121,7 +100,7 @@
             (function() {
                 var StackCards = function(element) {
                     this.element = element;
-                    this.items = document.querySelector("js-stack-cards__item");
+                    this.items = this.element.querySelectorAll(".js-stack-cards__item");
                     this.scrollingFn = false;
                     this.scrolling = false;
                     initStackCardsEffect(this);
@@ -184,7 +163,7 @@
 
                     for (var i = 0; i < element.items.length; i++) {
                         if (isNaN(element.marginY)) {
-                            element.items[i].style.transform = "none;";
+                            element.items[i].style.transform = "none";
                         } else {
                             element.items[i].style.transform = "translateY(" + element.marginY * i + "px)";
                         }
