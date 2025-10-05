3effd2d9e9c0c9401d08b82b271e4c2ed1805f98
Szamansaju <szamansaju@gmail.com>
2025-10-05 12:38:34 +0600

    Some changes done

--- resources/views/frontend/pages/homePage_category.blade.php
+++ resources/views/frontend/pages/homePage_category.blade.php
@@ -10,187 +10,187 @@
                         <div class="d-flex align-items-center">
                             <h1 class="text-black design-title">
                                 {{ optional($categoryOne)->name }}
-                            </h1>
-                            <span class="line ms-2"></span>
-                        </div>
-                        <p class="pt-3 text-black">
-                            {!! optional($categoryOne)->description !!}
-                        </p>
-                        <div class="mt-4 row">
+</h1>
+<span class="line ms-2"></span>
+</div>
+<p class="pt-3 text-black">
+    {!! optional($categoryOne)->description !!}
+</p>
+<div class="mt-4 row">
 
-                            @foreach (optional($categoryOne)->children as $header_category_child)
-                                <div class="col-lg-6">
+    @foreach (optional($categoryOne)->children as $header_category_child)
+    <div class="col-lg-6">
 
-                                    <div class="mb-3 shape_side_box">
-                                        <a href="{{ route('project') }}" class="shadow-sm btn btn-white arrow-btns">
-                                            <i class="fa-solid fa-chevron-right pe-2"></i>
-                                        </a>
-                                        <a href="{{ route('project') }}"
-                                            class="shadow-sm btn btn-white services-btn w-75 text-start">
-                                            {{ $header_category_child->name }}
-                                        </a>
-                                    </div>
+        <div class="mb-3 shape_side_box">
+            <a href="{{ route('project') }}" class="shadow-sm btn btn-white arrow-btns">
+                <i class="fa-solid fa-chevron-right pe-2"></i>
+            </a>
+            <a href="{{ route('project') }}"
+                class="shadow-sm btn btn-white services-btn w-75 text-start">
+                {{ $header_category_child->name }}
+            </a>
+        </div>
 
-                                </div>
-                            @endforeach
+    </div>
+    @endforeach
 
 
-                        </div>
-                    </div>
+</div>
+</div>
+</div>
+<div class="col-lg-6">
+    <div>
+        <img class="mobile-imgs"
+            src="{{ !empty(optional($categoryOne)->image) ? url('storage/' . optional($categoryOne)->image) : 'https://ui-avatars.com/api/?name=' . urlencode(optional($categoryOne)->name) }}"
+            alt="{{ optional($categoryOne)->name }}" />
+    </div>
+</div>
+</div>
+
+</div>
+</section>
+<!-- Feature Section End -->
+
+<!-- Feature Section Start -->
+<section>
+    <div class="container py-5">
+        <div class="row align-items-center">
+
+            <div class="col-lg-6">
+                <div>
+                    <img class="img-fluid"
+                        src="{{ !empty(optional($categoryTwo)->image) ? url('storage/' . optional($categoryTwo)->image) : '' }}"
+                        alt="{{ optional($categoryTwo)->name }}" alt="Graphics Services" />
                 </div>
-                <div class="col-lg-6">
-                    <div>
-                        <img class="mobile-imgs"
-                            src="{{ !empty(optional($categoryOne)->image) ? url('storage/' . optional($categoryOne)->image) : 'https://ui-avatars.com/api/?name=' . urlencode(optional($categoryOne)->name) }}"
-                            alt="{{ optional($categoryOne)->name }}" />
+            </div>
+
+            <div class="col-lg-6">
+                <div class="">
+                    <div class="d-flex align-items-center">
+                        <h1 class="grp-title">{{ optional($categoryTwo)->name }}</h1>
+                        <span class="line ms-2"></span>
+                    </div>
+                    <p class="pt-3 grp-para">
+                        {!! optional($categoryTwo)->description !!}
+                    </p>
+                    <div class="mt-4 row">
+
+                        @foreach (optional($categoryTwo)->children as $header_category_child_two)
+                        <div class="col-lg-6">
+
+                            <div class="mb-3 shape_side_box">
+                                <button class="shadow-sm btn btn-white arrow-btns shape_side_btn">
+                                    <i class="fa-solid fa-chevron-right pe-2"></i>
+                                </button>
+                                <button
+                                    class="shadow-sm btn btn-white services-btn w-75 text-start shape_side_btn">
+                                    {{ $header_category_child_two->name }}
+                                </button>
+                            </div>
+
+
+                        </div>
+                        @endforeach
+
+
                     </div>
                 </div>
             </div>
 
         </div>
-    </section>
-    <!-- Feature Section End -->
-
-    <!-- Feature Section Start -->
-    <section>
-        <div class="container py-5">
-            <div class="row align-items-center">
-
-                <div class="col-lg-6">
-                    <div>
-                        <img class="img-fluid"
-                            src="{{ !empty(optional($categoryTwo)->image) ? url('storage/' . optional($categoryTwo)->image) : '' }}"
-                            alt="{{ optional($categoryTwo)->name }}" alt="Graphics Services" />
-                    </div>
-                </div>
-
-                <div class="col-lg-6">
-                    <div class="">
-                        <div class="d-flex align-items-center">
-                            <h1 class="grp-title">{{ optional($categoryTwo)->name }}</h1>
-                            <span class="line ms-2"></span>
-                        </div>
-                        <p class="pt-3 grp-para">
-                            {!! optional($categoryTwo)->description !!}
-                        </p>
-                        <div class="mt-4 row">
-
-                            @foreach (optional($categoryTwo)->children as $header_category_child_two)
-                                <div class="col-lg-6">
-
-                                    <div class="mb-3 shape_side_box">
-                                        <button class="shadow-sm btn btn-white arrow-btns shape_side_btn">
-                                            <i class="fa-solid fa-chevron-right pe-2"></i>
-                                        </button>
-                                        <button
-                                            class="shadow-sm btn btn-white services-btn w-75 text-start shape_side_btn">
-                                            {{ $header_category_child_two->name }}
-                                        </button>
-                                    </div>
-
-
-                                </div>
-                            @endforeach
-
-
-                        </div>
-                    </div>
-                </div>
-
-            </div>
-        </div>
-    </section>
-    <!-- Feature Section End -->
+    </div>
+</section>
+<!-- Feature Section End -->
 </div> --}}
 
 <div class="shape-container" id="section1">
     <!-- Feature Section Start -->
     @if (!empty(optional($categoryOne)->name) || !empty(optional($categoryOne)->description))
-        <section>
-            <div class="container py-0 py-lg-5">
-                <div class="row align-items-center">
-                    <div class="col-lg-6">
-                        <div class="py-5">
-                            <div class="d-flex align-items-center">
-                                <h1 class="text-black design-title">
-                                    {{ optional($categoryOne)->name ?? 'Default Category One' }}
-                                </h1>
-                                <span class="line ms-2"></span>
-                            </div>
-                            <p class="pt-3 text-black">
-                                {!! optional($categoryOne)->description ?? 'Default Description' !!}
-                            </p>
-                            <div class="mt-4 row">
-                                @foreach (optional($categoryOne)->children ?? [] as $header_category_child)
-                                    <div class="col-lg-6">
-                                        <div class="mb-3 shape_side_box">
-                                            <a href="{{ route('project') }}" class="shadow-sm btn btn-white arrow-btns">
-                                                <i class="fa-solid fa-chevron-right pe-2"></i>
-                                            </a>
-                                            <a href="{{ route('project') }}"
-                                                class="shadow-sm btn btn-white services-btn w-75 text-start">
-                                                {{ $header_category_child->name }}
-                                            </a>
-                                        </div>
-                                    </div>
-                                @endforeach
-                            </div>
+    <section>
+        <div class="container py-0 py-lg-5">
+            <div class="row align-items-center">
+                <div class="col-lg-6">
+                    <div class="py-5">
+                        <div class="d-flex align-items-center">
+                            <h1 class="text-black design-title">
+                                {{ optional($categoryOne)->name ?? 'Default Category One' }}
+                            </h1>
+                            <span class="line ms-2"></span>
                         </div>
-                    </div>
-                    <div class="col-lg-6">
-                        <div>
-                            <img class="mobile-imgs"
-                                src="{{ !empty(optional($categoryOne)->image) ? url('storage/' . optional($categoryOne)->image) : 'https://ui-avatars.com/api/?name=' . urlencode(optional($categoryOne)->name ?? 'Default Category One') }}"
-                                alt="{{ optional($categoryOne)->name ?? 'Default Category One' }}" />
+                        <p class="pt-3 text-black">
+                            {!! optional($categoryOne)->description ?? 'Default Description' !!}
+                        </p>
+                        <div class="mt-4 row">
+                            @foreach (optional($categoryOne)->children ?? [] as $header_category_child)
+                            <div class="col-lg-6">
+                                <div class="mb-3 shape_side_box">
+                                    <a href="{{ route('project') }}" class="shadow-sm btn btn-white arrow-btns">
+                                        <i class="fa-solid fa-chevron-right pe-2"></i>
+                                    </a>
+                                    <a href="{{ route('project') }}"
+                                        class="shadow-sm btn btn-white services-btn w-75 text-start">
+                                        {{ $header_category_child->name }}
+                                    </a>
+                                </div>
+                            </div>
+                            @endforeach
                         </div>
                     </div>
                 </div>
+                <div class="col-lg-6">
+                    <div>
+                        <img class="mobile-imgs img-fluid"
+                            src="{{ !empty(optional($categoryOne)->image) ? url('storage/' . optional($categoryOne)->image) : 'https://ui-avatars.com/api/?name=' . urlencode(optional($categoryOne)->name ?? 'Default Category One') }}"
+                            alt="{{ optional($categoryOne)->name ?? 'Default Category One' }}" />
+                    </div>
+                </div>
             </div>
-        </section>
+        </div>
+    </section>
     @endif
     <!-- Feature Section End -->
 
     <!-- Feature Section Start -->
     @if (!empty(optional($categoryTwo)->name) || !empty(optional($categoryTwo)->description))
-        <section>
-            <div class="container py-5">
-                <div class="row align-items-center">
-                    <div class="col-lg-6">
-                        <div>
-                            <img class="img-fluid"
-                                src="{{ !empty(optional($categoryTwo)->image) ? url('storage/' . optional($categoryTwo)->image) : '' }}"
-                                alt="{{ optional($categoryTwo)->name ?? 'Default Category Two' }}" />
-                        </div>
+    <section>
+        <div class="container py-5">
+            <div class="row align-items-center">
+                <div class="col-lg-6">
+                    <div>
+                        <img class="img-fluid"
+                            src="{{ !empty(optional($categoryTwo)->image) ? url('storage/' . optional($categoryTwo)->image) : '' }}"
+                            alt="{{ optional($categoryTwo)->name ?? 'Default Category Two' }}" />
                     </div>
-                    <div class="col-lg-6">
-                        <div>
-                            <div class="d-flex align-items-center">
-                                <h1 class="grp-title">{{ optional($categoryTwo)->name ?? 'Default Category Two' }}</h1>
-                                <span class="ms-2 line-white"></span>
+                </div>
+                <div class="col-lg-6">
+                    <div>
+                        <div class="d-flex align-items-center">
+                            <h1 class="grp-title">{{ optional($categoryTwo)->name ?? 'Default Category Two' }}</h1>
+                            <span class="line-white ms-2"></span>
+                        </div>
+                        <p class="pt-3 grp-para">
+                            {!! optional($categoryTwo)->description ?? 'Default Description' !!}
+                        </p>
+                        <div class="mt-4 row">
+                            @foreach (optional($categoryTwo)->children ?? [] as $header_category_child_two)
+                            <div class="col-lg-6">
+                                <div class="mb-3 shape_side_box">
+                                    <a href="{{ route('project') }}" class="shadow-sm btn btn-white arrow-btns shape_side_btn">
+                                        <i class="fa-solid fa-chevron-right pe-2"></i>
+                                    </a>
+                                    <a href="{{ route('project') }}"
+                                        class="shadow-sm btn btn-white services-btn w-75 text-start shape_side_btn">
+                                        {{ $header_category_child_two->name }}
+                                    </a>
+                                </div>
                             </div>
-                            <p class="pt-3 grp-para">
-                                {!! optional($categoryTwo)->description ?? 'Default Description' !!}
-                            </p>
-                            <div class="mt-4 row">
-                                @foreach (optional($categoryTwo)->children ?? [] as $header_category_child_two)
-                                    <div class="col-lg-6">
-                                        <div class="mb-3 shape_side_box">
-                                            <a href="{{ route('project') }}" class="shadow-sm btn btn-white arrow-btns shape_side_btn">
-                                                <i class="fa-solid fa-chevron-right pe-2"></i>
-                                            </a>
-                                            <a href="{{ route('project') }}"
-                                                class="shadow-sm btn btn-white services-btn w-75 text-start shape_side_btn">
-                                                {{ $header_category_child_two->name }}
-                                            </a>
-                                        </div>
-                                    </div>
-                                @endforeach
-                            </div>
+                            @endforeach
                         </div>
                     </div>
                 </div>
             </div>
-        </section>
+        </div>
+    </section>
     @endif
     <!-- Feature Section End -->
 </div>
