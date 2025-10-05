f0193143e31879c21b881d792b4fe97db1c6acc2
Szamansaju <szamansaju@gmail.com>
2025-10-05 12:43:28 +0600

    Mobile Responsive Done

--- resources/views/frontend/pages/homePage_single_category.blade.php
+++ resources/views/frontend/pages/homePage_single_category.blade.php
@@ -5,7 +5,7 @@
                 <div class="col-lg-6">
                     <div class="py-5">
                         <div class="d-flex align-items-center">
-                            <h1 class="design-title text-black">
+                            <h1 class="text-black design-title">
                                 {{ optional($categoryThree)->name }}
                             </h1>
                             @if (!empty(optional($categoryThree)->name))
@@ -15,18 +15,18 @@
                         <p class="pt-3 text-black">
                             {!! optional($categoryThree)->description !!}
                         </p>
-                        <div class="row mt-4">
+                        <div class="mt-4 row">
 
                             @if (!empty(optional($categoryThree)->children))
                                 @foreach (optional($categoryThree)->children as $header_category_child_three)
                                     <div class="col-lg-6">
 
                                         <div class="mb-3 shape_side_box">
-                                            <a href="{{ route('project') }}" class="btn btn-white shadow-sm arrow-btns">
+                                            <a href="{{ route('project') }}" class="shadow-sm btn btn-white arrow-btns">
                                                 <i class="fa-solid fa-chevron-right pe-2"></i>
                                             </a>
                                             <a href="{{ route('project') }}"
-                                                class="btn btn-white shadow-sm services-btn w-75 text-start">
+                                                class="shadow-sm btn btn-white services-btn w-75 text-start">
                                                 {{ $header_category_child_three->name }}
                                             </a>
                                         </div>
@@ -41,7 +41,7 @@
                 </div>
                 <div class="col-lg-6">
 
-                    <img class="mobile-imgs"
+                    <img class="mobile-imgs img-fluid"
                         src="{{ !empty(optional($categoryThree)->image) ? url('storage/' . optional($categoryThree)->image) : '' }}"
                         alt="{{ optional($categoryThree)->name }}" />
 
